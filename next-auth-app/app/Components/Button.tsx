58d0104d4c81b50f666595967d801ff49645015e
Seth Keenan <seth.j.keenan@gmail.com>
2025-06-13 17:08:48 -0500

    Added OAuth Log in via GitHub

---
+++ next-auth-app/app/Components/Button.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+interface ButtonProps {
+  onClick?: () => void
+  children?: React.ReactNode
+  className?: string
+}
+
+export const Button: React.FC<ButtonProps> = ({ onClick, children, className = '' }) => {
+  
+    const baseClass = "px-4 py-2 bg-amber-600 text-white rounded hover:bg-amber-800 transition: duration-200"
+  
+    return (
+    <button onClick={onClick} className={`${baseClass} ${className}`}>
+      {children}
+    </button>
+  )
+}
---
+++ next-auth-app/app/page.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { Button } from "./Components/Button";
+import { useSession, signIn, signOut } from "next-auth/react";
+
+export default function Home() {
+
+  const { data: session, status } = useSession();
+  
+  const isLoggedIn = status === "authenticated";
+  
+  return (
+    <div className={`flex flex-col items-center justify-center h-screen`}>
+      <h1 className={`text-5xl`}>
+        Next Chat
+      </h1>
+      {isLoggedIn  
+      ?
+      <>
+        <p>You are logged in as {session?.user?.name}.</p>
+        <Button onClick={() => signOut({ callbackUrl: "/" })}>Log out</Button>
+      </>
+      :
+      <>
+        <p>You are not logged in.</p>
+        <Button onClick={() => signIn("github", { redirectTo: "/" })}>Log in with GitHub</Button>
+      </>
+    }
+    </div>
+  );
+}
